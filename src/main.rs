5daed3a9b5b52b14b073febedc80e4a01adf4435
Greg Leonard <gregjamesleonard@gmail.com>
2022-03-23 02:03:58 +0000

    Update light kelvin and tests

--- src/main.rs
+++ src/main.rs
@@ -66,10 +66,31 @@ async fn get_weather() {
 
     let body = resp.text().await.unwrap();
     let open_weather: OpenWeather = serde_json::from_str(&body).unwrap();
-    let bri = get_brightness_from_sunrise_sunset(open_weather).await;
+    let bri = get_brightness_from_weather(open_weather).await;
 }
 
-async fn get_brightness_from_sunrise_sunset(current_weather: OpenWeather) -> f32 {
+fn get_light_warmth_from_time_of_day(current_weather: OpenWeather) -> u16 {
+    let sunrise = current_weather.sys.sunrise as i64;
+    let sunset = current_weather.sys.sunset as i64;
+    let current_time = current_weather.dt as i64;
+    let midday = (sunrise + sunset) / 2;
+
+    if sunrise < current_time && current_time < sunset {
+        let s = match current_time < midday {
+            true => sunrise,
+            false => sunset,
+        };
+        let a = (current_time - s).abs();
+        let b = (midday - s).abs();
+        let k = (4000 * a) / b + 2000;
+        k as u16
+    } else {
+        // TODO
+        0
+    }
+}
+
+async fn get_brightness_from_weather(current_weather: OpenWeather) -> f32 {
     let sunrise = current_weather.sys.sunrise;
     let sunset = current_weather.sys.sunset;
     let now = current_weather.dt;
@@ -121,61 +142,86 @@ async fn main() -> std::io::Result<()> {
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[macro_use]
     use dotenv::dotenv;
+
+    fn get_mock_open_data() -> OpenWeather {
+        serde_json::from_str(
+            r#"{
+            "base":"stations",
+            "clouds":{
+                "all":100
+            },
+            "cod":200,
+            "coord":{
+                "lat":51.2766,
+                "lon":-0.8422
+            },
+            "dt":1641045600,
+            "id":2649322,
+            "main":{
+                "feels_like":279.21,
+                "humidity":76,
+                "pressure":1010,
+                "temp":280.86,
+                "temp_max":282.38,
+                "temp_min":279.75
+            },
+            "name":"Fleet",
+            "sys":{
+                "country":"GB",
+                "id":2037306,
+                "sunrise":1641024000,
+                "sunset":1641067200,
+                "type":2
+            },
+            "timezone":0,
+            "visibility":10000,
+            "weather":
+            [
+                {
+                    "description":"overcast clouds",
+                    "icon":"04n",
+                    "id":804,
+                    "main":"Clouds"
+                }
+            ],
+            "wind":{
+                "deg":180,
+                "speed":2.57
+            }
+        }"#,
+        )
+        .unwrap()
+        // sunrise: Sat Jan 01 2022 08:00:00 GMT+0000
+        // sunset:  Sat Jan 01 2022 20:00:00 GMT+0000
+        // dt:      Sat Jan 01 2022 14:00:00 GMT+0000
+    }
 
     #[actix_rt::test]
     async fn test_get_weather() {
-        let mock_open_data: OpenWeather = serde_json::from_str(
-            r#"{
-                "base":"stations",
-                "clouds":{
-                    "all":100
-                },
-                "cod":200,
-                "coord":{
-                    "lat":51.2766,
-                    "lon":-0.8422
-                },
-                "dt":1647205200,
-                "id":2649322,
-                "main":{
-                    "feels_like":279.21,
-                    "humidity":76,
-                    "pressure":1010,
-                    "temp":280.86,
-                    "temp_max":282.38,
-                    "temp_min":279.75
-                },
-                "name":"Fleet",
-                "sys":{
-                    "country":"GB",
-                    "id":2037306,
-                    "sunrise":1647172800,
-                    "sunset":1647216000,
-                    "type":2
-                },
-                "timezone":0,
-                "visibility":10000,
-                "weather":
-                [
-                    {
-                        "description":"overcast clouds",
-                        "icon":"04n",
-                        "id":804,
-                        "main":"Clouds"
-                    }
-                ],
-                "wind":{
-                    "deg":180,
-                    "speed":2.57
-                }
-            }"#,
-        )
-        .unwrap();
+        let mock_open_data: OpenWeather = get_mock_open_data();
         dotenv().ok();
-        get_amount_through_day(mock_open_data).await;
+        get_brightness_from_weather(mock_open_data).await;
         assert_eq!(1, 1);
+    }
+
+    #[test]
+    fn test_get_light_warmth_from_time_of_day() {
+        let expected: [(u32, u16, &str); 8] = [
+            (1641024000, 0, "08:00"),
+            (1641027600, 2666, "09:00"),
+            (1641031200, 3333, "10:00"),
+            (1641038400, 4666, "12:00"),
+            (1641045600, 6000, "14:00"),
+            (1641052800, 4666, "16:00"),
+            (1641060000, 3333, "18:00"),
+            (1641067200, 0, "20:00"),
+        ];
+        for (epoch, kelvin, time) in expected {
+            let mut mock_open_data = get_mock_open_data();
+            mock_open_data.dt = epoch;
+            let result = get_light_warmth_from_time_of_day(mock_open_data);
+            println!("time: {time}, expected_kelvin: {kelvin}, calculated_kelvin: {result}");
+        }
     }
 }
